e8f4c8714c215e50eb655ee24c1d10bea1989247
David-Rey <71460537+David-Rey@users.noreply.github.com>
2021-04-18 19:40:14 -0400

    Add files via upload

--- AeroMain.m
+++ AeroMain.m
@@ -38,43 +38,42 @@
 
 %%% File Info %%%
 %Filename					~ lines		(~ sloc)	size
-%AeroMain.m					3 lines* 	(3 sloc)*  	1.32 KB
-%GUIinput.m					244 lines	(224 sloc)  14.5 KB
-%calculateRunTime.m			12 lines 	(12 sloc)  	453 Bytes
-%cartesionToSpherical.m		9 lines 	(8 sloc)  	204 Bytes
-%CDUpdate.m
-%generateRandomValues.m		6 lines 	(6 sloc)  	229 Bytes
-%getAbsVectors.m			7 lines 	(6 sloc)  	282 Bytes
-%getAtmosphere.m			77 lines 	(68 sloc)  	4.48 KB
-%getAtmosphereAtHeight.m	35 lines 	(34 sloc)  	1.16 KB
-%getCD.m					20 lines 	(20 sloc)  	476 Bytes
-%getExactScreenSize.m
-%getGravity.m				8 lines 	(8 sloc) 	164 Bytes
-%getLocalVectors.m			7 lines 	(6 sloc)  	286 Bytes
-%getRelHumidity.m			15 lines 	(11 sloc)  	324 Bytes
-%getStandardATM.m			42 lines 	(36 sloc)  	1.39 KB
-%getThrust.m				25 lines 	(25 sloc)  	877 Bytes
-%getUnitVector.m			6 lines 	(6 sloc)  	139 Bytes
-%locationUpdate.m			7 lines 	(7 sloc)  	228 Bytes
-%plotCircle3D.m				16 lines 	(15 sloc)  	539 Bytes
-%plotEarth.m				25 lines 	(21 sloc)  	974 Bytes
-%plotOutput.m				213 lines 	(197 sloc)  8.17 KB
-%plotPointGUI.m				4 lines 	(4 sloc)  	369 Bytes
-%run3DSimulation.m			247 lines 	(228 sloc)  10.5 KB
-%runSimulation.m			170 lines 	(153 sloc)  6.76 KB
-%sphericalToCartesion.m		9 lines 	(8 sloc)  	211 Bytes
-%standardATMUpdate.m		7 lines 	(7 sloc)  	232 Bytes
-%staticEnvUpdate.m			7 lines 	(7 sloc)  	226 Bytes
-%UIanimate.m
-%updateGravityCheckBox.m	7 lines 	(7 sloc)  	148 Bytes
-%updateLonLat.m
-%updateObjectValues.m		13 lines 	(12 sloc)  	528 Bytes
-%zoomEarth.m				7 lines 	(7 sloc)  	185 Bytes
+%AeroMain.m					4 lines* 	(4 sloc)*  	4.24 KB
+%GUIinput.m					292 lines	(269 sloc)  17.3 KB
+%calculateRunTime.m			15 lines 	(14 sloc)  	556 Bytes
+%cartesionToSpherical.m		11 lines 	(9 sloc)  	265 Bytes
+%CDUpdate.m					9 lines		(8 sloc)	183 Bytes
+%generateRandomValues.m		8 lines 	(7 sloc)  	271 Bytes
+%getAbsVectors.m			9 lines 	(7 sloc)  	325 Bytes
+%getAtmosphere.m			80 lines 	(71 sloc)  	4.71 KB
+%getAtmosphereAtHeight.m	37 lines 	(35 sloc)  	1.3 KB
+%getCD.m					22 lines 	(21 sloc)  	521 Bytes
+%getExactScreenSize.m		15 lines	(15 sloc)	1008 Bytes
+%getGravity.m				10 lines 	(9 sloc) 	196 Bytes
+%getLocalVectors.m			9 lines 	(7 sloc)  	340 Bytes
+%getRelHumidity.m			8 lines 	(7 sloc)  	276 Bytes
+%getStandardATM.m			39 lines 	(34 sloc)  	1.43 KB
+%getThrust.m				32 lines 	(30 sloc)  	1.1 KB
+%getUnitVector.m			8 lines 	(7 sloc)  	205 Bytes
+%locationUpdate.m			9 lines 	(8 sloc)  	273 Bytes
+%plotCircle3D.m				15 lines 	(14 sloc)  	573 Bytes
+%plotEarth.m				27 lines 	(22 sloc)  	1.04 KB
+%plotOutput.m				214 lines 	(197 sloc)  8.22 KB
+%plotPointGUI.m				6 lines 	(5 sloc)  	369 Bytes
+%run3DSimulation.m			252 lines 	(230 sloc)  10.6 KB
+%runSimulation.m			165 lines 	(147 sloc)  6.6 KB
+%sphericalToCartesion.m		11 lines 	(9 sloc)  	272 Bytes
+%standardATMUpdate.m		9 lines 	(8 sloc)  	277 Bytes
+%staticEnvUpdate.m			9 lines 	(8 sloc)  	271 Bytes
+%UIanimate.m				19 lines 	(18 sloc)  	463 Bytes
+%updateGravityCheckBox.m	9 lines 	(8 sloc)  	193 Bytes
+%updateLonLat.m				22 lines 	(21 sloc)  	571 Bytes
+%updateObjectValues.m		15 lines 	(13 sloc)  	575 Bytes
+%zoomEarth.m				9 lines 	(8 sloc)  	230 Bytes
 
-%Total:						1248 lines	(1146 sloc)	55.7 KB
+% *Comments not counted in AeroMain.m
 
-%note: totals taken on March 26 2020. Data may not be up to date. 
-%*comments not counted in AeroMain.m
+%Total:						1399 lines	(1270 sloc)	64.6 KB
 
 clear
 
